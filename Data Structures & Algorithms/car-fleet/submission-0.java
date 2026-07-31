cc1351ae44d8508564c5f34bc8ff808d48e2bf71
Menka Shah <91521858+Menka443@users.noreply.github.com>
2026-07-31 16:08:16 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int carFleet(int target, int[] position, int[] speed) {
+          
+       int n = position.length;
+
+       int cars[][] = new int[n][2];
+       for(int i = 0; i < n; i++){
+        cars[i][0] = position[i];
+        cars[i][1] = speed[i];
+        
+       }
+// : Sort by position (descending)
+        Arrays.sort(cars, (a, b) -> b[0] - a[0]);
+
+
+       Stack<Double> stack = new Stack<>();
+// : Traverse and calculate time
+       for (int i = 0; i < n; i++) {
+
+            double time = (double)(target - cars[i][0]) / cars[i][1];
+
+            if (stack.isEmpty() || time > stack.peek()) {
+                stack.push(time);
+            }
+        }
+  return stack.size();
+        
+    }
+  
+}
